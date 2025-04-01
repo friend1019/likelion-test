2ca24a897d5172bac99a5c5d9f69d15455f0abe9
LeeKangHyun-lab <ka09068@gmail.com>
2025-04-01 19:24:47 +0900

    03_25 과제제출

---
+++ src/main/java/org/likelion_hsu/likelion_day1/LikelionDay1Application.java
@@ -0,0 +1,13 @@
+package org.likelion_hsu.likelion_day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionDay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionDay1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/likelion_hsu/likelion_day1/LikelionDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelion_hsu.likelion_day1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionDay1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
