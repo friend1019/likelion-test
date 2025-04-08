38c355347f8e49964f280fe5ff9718cace0742fa
김형석 <gw904202@gmail.com>
2025-04-09 01:51:28 +0900

    04_08 수업내용

---
+++ 04_08.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const images=['image_likelion.jpg','img2.jpg','img3.jpg','img4.jpg'];
+    let currentIndex =0;
+
+    const imageElement = document.getElementById('slider-image');
+    const counterElement = document.getElementById('image-counter');
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');    
+
+    function updateSlider(){
+        imageElement.src=images[currentIndex];
+        counterElement.textContent=`${String(currentIndex+1).padStart(2,'0')}/${String(images.length).padStart(2,'0')}`;
+
+    }
+
+    prevBtn.addEventListener('click', () =>{
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        updateSlider();
+    });
+
+    nextBtn.addEventListener('click', () =>{
+        currentIndex = (currentIndex + 1) % images.length;
+        updateSlider();
+    });
+
+    updateSlider(); // 초기 이미지 및 카운터 업데이트
+
+});
